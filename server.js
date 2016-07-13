9adcca82fe0e0a95add503e0d3749b8c0fb7ddd6
Huang, Waylon (wh186y) <wh186y@att.com>
2016-07-13 15:20:03 -0700

    FINALLY FIXED IT - RENAMING THINGS IS EVIL

--- server.js
+++ server.js
@@ -9,42 +9,40 @@
   var hash = require('bcrypt-nodejs');
   var path = require('path');
   var passport = require('passport');
-  var localStrategy = require('passport-local').Strategy;
-  var express = require("express");
-  
+  var localStrategy = require('passport-local' ).Strategy;
+
   var app = express();
 
   // Set up parsing of the body as a JSON object
-  var bodyParser = require('body-parser');
   app.use(bodyParser.json());
 
   mongoose.connect('mongodb://localhost:27017/blackwater');
   var Schema = mongoose.Schema;
-  // user schema/model
-  var User = require('./models/user.model.js');
+  var User = require('./models/usermodel.js');
+
+  // require routes
   var exampleRoute = require('./routes/example.route.js');
   var galleryImageRoute = require('./routes/gallery-image.route.js');
-  var routes = require('./routes/api.route.js');
+  var routes = require('./routes/apiroute.js');
 
-  // Set up a static resources director
   app.use(express.static('public'));
   app.use('/examples', exampleRoute);
   app.use('/galleryImages', galleryImageRoute);
 
   // define middleware
-  //app.use(express.static(path.join(__dirname, '.')));
+  app.use(express.static(path.join(__dirname, './client')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(require('express-session')({
-      secret: 'fantabulous narwhal',
+      secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false
   }));
   app.use(passport.initialize());
   app.use(passport.session());
-  //app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   // configure passport
   passport.use(new localStrategy(User.authenticate()));
@@ -55,8 +53,9 @@
   app.use('/user/', routes);
 
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './', 'index.html'));
+    res.sendFile(path.join(__dirname, './client', 'index.html'));
   });
+
 
   // If no route is found, send a 404 error
   app.use(function(req, res) {
@@ -65,6 +64,6 @@
 
   // Start the server
   app.listen(3000, function() {
-    console.log("Heroes on the Water listening on port 3000");
+    console.log("Example app listening on port 3000");
   });
 })();
