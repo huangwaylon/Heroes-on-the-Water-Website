6b65510e5bf28faa94c6926d24a6446259efcf32
ALBANY <ka392d@att.com>
2016-07-19 13:59:22 -0700

    mailbox skeleton page

---
+++ client/modules/how/controllers/mailbox.route.ctrl.js
@@ -0,0 +1,10 @@
+(function() {
+  angular.module('app.how').controller('mailboxRouteCtrl',
+      function($log) {
+        $log.debug('Initializing mailboxRouteCtrl');
+
+        var self = this;
+
+        this.newExample = {};
+      });
+})();
