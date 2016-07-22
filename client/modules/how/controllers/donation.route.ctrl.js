780f89659a73a7a9a2c2002cbcdb187b1c05147a
PULGARIN <ap739s@att.com>
2016-07-22 06:57:30 -0700

    Added a controller for the donation form

---
+++ client/modules/how/controllers/donation.route.ctrl.js
@@ -0,0 +1,11 @@
+(function() {
+  angular.module('app.how').controller('donationRouteCtrl',
+      function($log, exampleService) {
+        $log.debug('Initializing donationRouteCtrl');
+
+        var self = this;
+
+        this.newExample = {};
+
+      });
+})();
