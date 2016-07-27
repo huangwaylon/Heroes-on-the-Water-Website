403cb3ddf66281eacece2fc573b00a9ca6444002
ALBANY <ka392d@att.com>
2016-07-27 11:37:48 -0700

    model and route changes for chapter field in inventory

--- routes/inv.route.js
+++ routes/inv.route.js
@@ -7,6 +7,7 @@ var Q = require('q');
 inventory_router.post('/add', function(req, res) {
   var newItem = new Inventory({
                   name: req.body.i_name,
+                  chapter: req.body.i_chapter,
                   description: req.body.i_description,
                   isUsed: req.body.i_isUsed
                 });
