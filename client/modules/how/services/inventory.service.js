8e3443de204b7a5c0e144115fdb50dbc972b941f
ALBANY <ka392d@att.com>
2016-07-27 16:13:20 -0700

    Inventory editing complete

--- client/modules/how/services/inventory.service.js
+++ client/modules/how/services/inventory.service.js
@@ -1,68 +1,91 @@
 (function() {
-  angular.module('app.how').factory('InvService',
-    ['$q', '$timeout', '$http',
-    function ($q, $timeout, $http) {
-      // return available functions for use in the controllers
-      return ({
-        add: add,
-        remove: remove,
-        all: all
-      });
+    angular.module('app.how').factory('InvService', ['$q', '$timeout', '$http',
+        function($q, $timeout, $http) {
+            // return available functions for use in the controllers
+            return ({
+                add: add,
+                remove: remove,
+                all: all,
+                update: update
+            });
 
-      function add(name, description, chapter, events, isUsed) {
-        // create a new instance of deferred
-        var deferred = $q.defer();
+            function add(name, description, chapter, events, isUsed) {
+                // create a new instance of deferred
+                var deferred = $q.defer();
 
-        // send a post request to the server
-        $http.post('/inventory/add',
-          {i_name: name,
-            i_description: description,
-            i_chapter: chapter,
-            i_events: events,
-            i_isUsed: isUsed})
-          // handle success
-          .success(function (data, status) {
-            if(status === 200 && data.status){
-              deferred.resolve();
-            } else {
-              deferred.reject();
+                // send a post request to the server
+                $http.post('/inventory/add', {
+                        i_name: name,
+                        i_description: description,
+                        i_chapter: chapter,
+                        i_events: events,
+                        i_isUsed: isUsed
+                    })
+                    // handle success
+                    .success(function(data, status) {
+                        if (status === 200 && data.status) {
+                            deferred.resolve();
+                        } else {
+                            deferred.reject();
+                        }
+                    })
+                    // handle error
+                    .error(function(data) {
+                        deferred.reject();
+                    });
+                // return promise object
+                return deferred.promise;
             }
-          })
-          // handle error
-          .error(function (data) {
-            deferred.reject();
-          });
-        // return promise object
-        return deferred.promise;
-      }
-      function remove(_id) {
-        // create a new instance of deferred
-        var deferred = $q.defer();
 
-        // send a post request to the server
-        $http.post('/inventory/remove',
-          {_id: _id})
-          // handle success
-          .success(function (data, status) {
-            if(status === 200 && data.status){
-              deferred.resolve();
-            } else {
-              deferred.reject();
+            function remove(_id) {
+                // create a new instance of deferred
+                var deferred = $q.defer();
+
+                // send a post request to the server
+                $http.post('/inventory/remove', { _id: _id })
+                    // handle success
+                    .success(function(data, status) {
+                        if (status === 200 && data.status) {
+                            deferred.resolve();
+                        } else {
+                            deferred.reject();
+                        }
+                    })
+                    // handle error
+                    .error(function(data) {
+                        deferred.reject();
+                    });
+                // return promise object
+                return deferred.promise;
             }
-          })
-          // handle error
-          .error(function (data) {
-            deferred.reject();
-          });
-        // return promise object
-        return deferred.promise;
-      }
 
-      function all(scope) {
-        return $http.get('/inventory/all').then(function(response) {
-          scope.allItems = response.data;
-        });
-      }
+            //Edits fields within an existing item in the DB
+            function update(item) {
+                var deferred = $q.defer();
 
-  }]);
+                $http.post('/inventory/update', item)
+                    // handle success
+                    .success(function(data, status) {
+                        if (status === 200 && data.status) {
+                            deferred.resolve();
+                        } else {
+                            deferred.reject();
+                        }
+                    })
+                    // handle error
+                    .error(function(data) {
+                        deferred.reject();
+                    });
+                // return promise object
+                return deferred.promise;
+            }
+
+            function all(scope) {
+                return $http.get('/inventory/all').then(function(response) {
+                    scope.allItems = response.data;
+                });
+            }
+
+        }
+    ]);
 })();
