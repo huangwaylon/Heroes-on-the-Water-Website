f7fe2ee3d5e6da4309f0e806bdf7b196b05ae5b9
Huang, Waylon (wh186y) <wh186y@att.com>
2016-07-29 10:11:10 -0700

    changes and fixes donors

--- client/modules/how/controllers/donation.route.ctrl.js
+++ client/modules/how/controllers/donation.route.ctrl.js
@@ -5,6 +5,7 @@
         $scope.allDonors = [];
         donationService.all($scope);
 
+        // Initialize scope variables
         $scope.success = false;
         $scope.error = false;
         $scope.errorMessage = "";
@@ -19,7 +20,11 @@
           $scope.errorMessage = "Not an Administrator!";
         }
 
+        // Set listeners
         $scope.$on("user_loaded", checkUserPermissions);
+        $scope.$on("user_login", setLogin);
+
+        // Check user permissions
         function checkUserPermissions() {
           if ($scope.user.account && ($scope.user.account == "Administrator" ||
                                       $scope.user.account == "Region Leader" ||
@@ -32,7 +37,11 @@
             $scope.errorMessage = "Not an Administrator!";
           }
         }
-
+        // After login, get user info again - happens on refresh
+        function setLogin() {
+          // Get user details after login
+          AuthService.hello($scope.user);
+        }
 
         // Function to add the new donor
         $scope.addDonor = function() {
@@ -60,5 +69,3 @@
         };
       });
 })();
-
-//Mandatory. Include additional functionality here.
